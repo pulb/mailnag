4b2861c5c8c051926bb2889844f56ee72138a6bd
Patrick Ulbrich <zulu99@gmx.net>
2012-08-18 15:31:14 +0200

    post release version bump

--- Mailnag/common/dist_cfg.py
+++ Mailnag/common/dist_cfg.py
@@ -29,7 +29,7 @@
 
 # Application version displayed in the 
 # about dialog of the config window.
-APP_VERSION = "0.4.2"
+APP_VERSION = "0.4.3"
 
 # The PACKAGE_NAME variable is used to configure
 # 1) the path where all app data (glade files, images) is loaded from
