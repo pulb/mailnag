bb5f3767d55eaf7a38a3b4a85b2f8bf0006a5225
Patrick Ulbrich <zulu99@gmx.net>
2013-07-14 20:16:06 +0200

    added libnotify plugin (not enabled by default)

---
+++ Mailnag/plugins/libnotifyplugin.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+# libnotifyplugin.py
+#
+# Copyright 2013 Patrick Ulbrich <zulu99@gmx.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+
+from gi.repository import Notify
+from common.plugins import Plugin, HookTypes
+from common.i18n import _
+
+plugin_defaults = { 'notification_mode' : '1' }
+
+
+class LibNotifyPlugin(Plugin):
+	def __init__(self):
+		# prevent flooding of the messaging tray
+		self.MAIL_LIST_LIMIT = 10
+		
+		# dict that tracks all notifications that need to be closed
+		self._notifications = {}
+		self._initialized = False
+		self._mails_added_hook = None
+		self._mails_removed_hook = None
+		
+	
+	def enable(self):
+		self._notifications = {}
+		
+		# initialize Notification
+		if not self._initialized:
+			Notify.init("Mailnag")
+			self._initialized = True
+		
+		def mails_added_hook(all_mails, new_mail_count):
+			config = self.get_config()
+			if config['notification_mode'] == 0:
+				# TODO : not supported by plugin API yet 
+				# self._notify_single(new_mails)
+				pass
+			else:
+				self._notify_summary(all_mails)
+		
+		def mails_removed_hook(remaining_mails):
+			if remaining_mails == 0:
+				# no mails (e.g. email client has been launched) -> close notifications
+				for n in self._notifications.itervalues():
+					n.close()
+				self._notifications = {}
+		
+		self._mails_added_hook = mails_added_hook
+		self._mails_removed_hook = mails_removed_hook
+		
+		controller = self.get_mailnag_controller()
+		
+		controller.hooks.register_hook_func(HookTypes.MAILS_ADDED, 
+			self._mails_added_hook)
+		controller.hooks.register_hook_func(HookTypes.MAILS_REMOVED, 
+			self._mails_removed_hook)
+		
+	
+	def disable(self):
+		controller = self.get_mailnag_controller()
+		
+		if self._mails_added_hook != None:
+			controller.hooks.unregister_hook_func(HookTypes.MAILS_ADDED,
+				self._mails_added_hook)
+			self._mails_added_hook = None
+		
+		if self._mails_removed_hook != None:
+			controller.hooks.unregister_hook_func(HookTypes.MAILS_REMOVED,
+				self._mails_removed_hook)
+			self._mails_removed_hook = None
+		
+		for n in self._notifications.itervalues():
+			n.close()
+		self._notifications = {}
+
+	
+	def get_manifest(self):
+		return (_("LibNotify notifications"),
+				_("Shows a popup when new mails arrive."),
+				"1.0",
+				"Patrick Ulbrich <zulu99@gmx.net>",
+				False)
+
+
+	def get_default_config(self):
+		return plugin_defaults
+	
+	
+	def has_config_ui(self):
+		return False
+	
+	
+	def get_config_ui(self):
+		# TODO : Add ui to specify the
+		# notification mode.
+		return None
+	
+	
+	def load_ui_from_config(self, config_ui):
+		pass
+	
+	
+	def save_ui_to_config(self, config_ui):
+		pass
+
+
+	def _notify_summary(self, mails):
+		summary = ""		
+		body = ""
+
+		if len(self._notifications) == 0:
+			self._notifications['0'] = self._get_notification(" ", None, None) # empty string will emit a gtk warning
+
+		ubound = len(mails) if len(mails) <= self.MAIL_LIST_LIMIT else self.MAIL_LIST_LIMIT
+
+		for i in range(ubound):
+			body += mails[i].sender + ":\n<i>" + mails[i].subject + "</i>\n\n"
+
+		if len(mails) > self.MAIL_LIST_LIMIT:
+			body += "<i>" + _("(and {0} more)").format(str(len(mails) - self.MAIL_LIST_LIMIT)) + "</i>"
+
+		if len(mails) > 1: # multiple new emails
+			summary = _("You have {0} new mails.").format(str(len(mails)))
+		else:
+			summary = _("You have a new mail.")
+
+		self._notifications['0'].update(summary, body, "mail-unread")
+		self._notifications['0'].show()
+	
+	
+	def _notify_single(self, mails):
+		for mail in mails:
+			n = self._get_notification(mail.sender, mail.subject, "mail-unread")
+			# notification_id = str(id(n))
+			# n.add_action("mark-as-read", _("Mark as read"), self._notification_action_handler, (mail, notification_id), None)			
+			n.show()
+			self._notifications[notification_id] = n
+
+
+	def _get_notification(self, summary, body, icon):
+		n = Notify.Notification.new(summary, body, icon)		
+		n.set_category("email")
+		# n.add_action("default", "default", self._notification_action_handler, None, None)
+
+		return n
