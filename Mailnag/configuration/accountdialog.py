d29d22547a0bf2ccddbe902f75fc6e423745637f
Patrick Ulbrich <zulu99@gmx.net>
2013-07-21 12:30:49 +0200

    removed unnecessary import

--- Mailnag/configuration/accountdialog.py
+++ Mailnag/configuration/accountdialog.py
@@ -25,7 +25,6 @@ from gi.repository import GLib, GdkPixbuf, Gtk
 from common.dist_cfg import PACKAGE_NAME
 from common.i18n import _
 from common.utils import get_data_file
-from common.accounts import Account
 
 class AccountDialog:
 	def __init__(self, parent, acc):
