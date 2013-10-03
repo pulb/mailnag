c7c926996b183a26b9635da798e8d67957b0e6f1
Patrick Ulbrich <zulu99@gmx.net>
2013-10-03 13:10:40 +0200

    subproc: added a further lock, comments and refactoring

--- Mailnag/common/subproc.py
+++ Mailnag/common/subproc.py
@@ -26,71 +26,93 @@ import threading
 import logging
 import time
 
-_lock = threading.Lock()
+# Note : All functions of this module *are* thread-safe.
+
+# Protects access to the proc dictionary.
+_proc_lock = threading.Lock()
+
+# Intended to prevent simultaneous execution of 
+# (start_subprocess, terminate_subprocesses), 
+# (terminate_subprocesses, terminate_subprocesses).
+_func_lock = threading.Lock()
+
+# Dictionary holding popen objects 
+# and associated thread objects.
 _procs = {}
 
 
+# Starts a subprocess and an associated thread that waits for
+# the subprocess to terminate (prevents zombie processes).
 def start_subprocess(args, shell = False, callback = None):
 	def thread():
 		t = threading.currentThread()
 		
 		try:
 			p = None
-			with _lock: p = _procs[t]
+			with _proc_lock: p = _procs[t]
 			retcode = p.wait()
 			
 			if callback != None:
 				callback(retcode)
 		finally:
-			with _lock: del _procs[t]
+			with _proc_lock: del _procs[t]
 	
-	p = None
-	pid = -1
-	try:
-		p = subprocess.Popen(args, shell = shell)
-	except: logging.exception('Caught an exception.')
+	with _func_lock:
+		p = None
+		pid = -1
+		try:
+			p = subprocess.Popen(args, shell = shell)
+		except: logging.exception('Caught an exception.')
 	
-	if p != None:
-		pid = p.pid
-		t = threading.Thread(target = thread)
-		with _lock:
-			_procs[t] = p
-			t.start()
+		if p != None:
+			pid = p.pid
+			t = threading.Thread(target = thread)
+			with _proc_lock:
+				_procs[t] = p
+				t.start()
 	
-	return pid
+		return pid
 
 
-def terminate_subprocesses():
-	threads = []
-	
-	with _lock:
-		if len(_procs) == 0:
-			return
+# Terminates all subprocesses that were started by
+# start_subprocess(). Subprocesses that don't terminate
+# within the timeframe (seconds) specified by the 
+# timeout argument, are sent a kill signal.
+def terminate_subprocesses(timeout = 3.0):
+	with _func_lock:
 		
-		for t, p in _procs.iteritems():
-			threads.append(t)
+		threads = []
+		
+		with _proc_lock:
+			if len(_procs) == 0:
+				return
+		
+			for t, p in _procs.iteritems():
+				threads.append(t)
 			
-			# Ask all runnig processes to terminate.
-			# This will also terminate threads waiting for p.wait().
-			# Note : terminate() does not block.
-			try: p.terminate()
-			except: logging.debug('p.terminate() failed')
+				# Ask all runnig processes to terminate.
+				# This will also terminate associated threads 
+				# waiting for p.wait().
+				# Note : terminate() does not block.
+				try: p.terminate()
+				except: logging.debug('p.terminate() failed')
 
-	# Start a watchdaog thread that will kill 
-	# all processes that didn't terminate within 3 seconds.
-	wd = _Watchdog(3.0)
-	wd.start()
+		# Start a watchdog thread that will kill 
+		# all processes that didn't terminate within <timeout> seconds.
+		wd = _Watchdog(timeout)
+		wd.start()
 	
-	# Wait for all threads to terminate
-	for t in threads:
-		t.join()
+		# Wait for all threads to terminate
+		for t in threads:
+			t.join()
 	
-	wd.stop()
+		wd.stop()
 	
-	if (not wd.triggered):
-		logging.info('All subprocesses exited normally.')
+		if (not wd.triggered):
+			logging.info('All subprocesses exited normally.')
 
 
+# Internal Watchdog class
 class _Watchdog(threading.Thread):
 	def __init__(self, timeout):
 		threading.Thread.__init__(self)
@@ -106,7 +128,7 @@ class _Watchdog(threading.Thread):
 		if not self._event.is_set():
 			logging.warning('Process termination took too long - watchdog starts killing...')
 			self.triggered = True
-			with _lock:
+			with _proc_lock:
 				for t, p in _procs.iteritems():
 					try:
 						# Kill process p and quit the thread 
