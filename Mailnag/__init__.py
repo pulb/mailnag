1ffe12ab0652faf4a0483fe8a720838b598f6b8c
Patrick Ulbrich <zulu99@gmx.net>
2019-11-02 11:54:46 +0100

    Remove uneccessary stuff from header in __init__.py

--- Mailnag/__init__.py
+++ Mailnag/__init__.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
-# __init__.py
-#
 # Copyright 2012 Patrick Ulbrich <zulu99@gmx.net>
 #
 # This program is free software; you can redistribute it and/or modify
